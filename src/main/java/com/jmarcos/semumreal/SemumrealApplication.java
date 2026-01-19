8e709b358dc98255bdeb7a53d15e3d673e83d3a6
Jose marcos <jose.marcos.efi@gmail.com>
2026-01-19 14:50:16 -0300

    Initial project structure with Java 21, Spring Boot, Liquibase and Postgres

---
+++ src/main/java/com/jmarcos/semumreal/SemumrealApplication.java
@@ -0,0 +1,13 @@
+package com.jmarcos.semumreal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemumrealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SemumrealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jmarcos/semumreal/SemumrealApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmarcos.semumreal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemumrealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
