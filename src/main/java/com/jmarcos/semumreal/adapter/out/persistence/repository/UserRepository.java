63fa4986def5a8396a3cb9c76a1a6c86e2d15f08
Jose marcos <jose.marcos.efi@gmail.com>
2026-01-21 12:46:14 -0300

    Feat: Add UserRepository interface for user data access with JPA

---
+++ src/main/java/com/jmarcos/semumreal/adapter/out/persistence/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.jmarcos.semumreal.adapter.out.persistence.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.jmarcos.semumreal.adapter.out.persistence.entity.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByEmail(String email);
+    boolean existsByEmail(String email);
+}
